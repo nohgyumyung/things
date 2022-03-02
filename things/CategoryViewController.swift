4d95b3d93ee891ccdcaeb3817dfe17e94eb21ef8
gyumyung <gyumyung1995@gmail.com>
2022-03-03 00:05:35 +0900

    build : storyboard to code base setting

---
+++ things/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  things
+//
+//  Created by 노규명 on 2022/03/03.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
